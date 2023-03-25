197949e7726f21e5607520c13001dcd012ee3d7d
Chris <chrismoneil99@gmail.com>
2023-03-25 13:39:02 -0400

    Added conditional to save resources

--- audio_diarization.py
+++ audio_diarization.py
@@ -4,7 +4,6 @@ import swifter
 import os
 
 
-
 def read_file(filename:str) -> pd.DataFrame:
     """
     reads in provided file containing links
@@ -20,7 +19,7 @@ def read_file(filename:str) -> pd.DataFrame:
     return pd.read_csv(filename) 
 
 
-def scrape_audio(scrape_links: pd.DataFrame, link_col: str) -> None:
+def scrape_audio(scrape_links: pd.DataFrame, link_col: str, output_d) -> None:
     """
     scrapes youtbube audio with the help of youtube dl
     
@@ -28,6 +27,7 @@ def scrape_audio(scrape_links: pd.DataFrame, link_col: str) -> None:
     scrape_links: data frame that contains 
     links for scraping
     link_col: column name for links
+    output_d: name of directory for output
 
     Returns:
     None
@@ -39,9 +39,28 @@ def scrape_audio(scrape_links: pd.DataFrame, link_col: str) -> None:
     stream_audio = lambda x: x.streams.filter(only_audio=True).first()
     df["audio"] = df["vids"].swifter.apply(stream_audio)
 
-    download_audio = lambda x: x.download(output_path="audio")
+    download_audio = lambda x: x.download(output_path=output_d)
     df["audio"].swifter.apply(download_audio)
     
 
-links = read_file("links.csv") 
-scrape_audio(scrape_links=links, link_col="Link")
+def diratzation(dir_list: list) -> None:
+    """
+    extracts subtitles and tagged speakers from
+    provided audio files
+
+    Prams:
+    dir_list: list of audio files
+
+    Returns: 
+    None
+
+    """
+
+
+links = read_file("links.csv")
+audio_file_dir = "audio"
+if (os.path.exists(audio_file_dir)) != True:
+    scrape_audio(scrape_links=links, 
+                link_col="Link", 
+                output_d=audio_file_dir)
+    
