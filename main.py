4b170b4c99b5fdf5861ac7cec8acce0c146aaaf0
Chris <chrismoneil99@gmail.com>
2023-03-30 18:09:51 -0400

    Implemented transcript pipeline

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from audio_diarization import read_file, scrape_audio, file_conversion, diratzation 
-from transcripts import get_video_id
+from transcripts import get_video_id, get_transcripts, read_in_trans
 
 def main()-> None:
     """
@@ -11,7 +11,7 @@ def main()-> None:
 
     Returns: None
     """
-    
+
     links = read_file("links.csv")
     audio_file_dir = "audio"
     scrape_audio(scrape_links=links, 
@@ -27,7 +27,16 @@ def main()-> None:
     rm_dir = "audiorttm"
     diratzation(file_list=wav_files, input_d=audio_wav_dir, rttm_dir=rm_dir)
     
-    ids = get_video_id(vid_links=links, link_col="Link")
+    ids = get_video_id(vid_links=links, link_col="Link") 
+    vid_titles = list(links["vidtitle"])
+    print(vid_titles)
+    
+    transcripts_dir="transcripts"
+    get_transcripts(vid_ids=ids, trans_dir=transcripts_dir, titles=vid_titles)
+    
+    json_files = os.listdir(transcripts_dir)
+    transcripts = read_in_trans(file_list=json_files, trans_dir=transcripts_dir) 
+    
 
 if __name__ == "__main__":
     main()
--- transcripts.py
+++ transcripts.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import swifter
- 
+import json
+import os
 
-def get_video_id(vid_links: pd.DataFrame, link_col="Link") -> list:
+
+def get_video_id(vid_links: pd.DataFrame, link_col="Link") -> pd.DataFrame:
     """
     parses video id from link 
 
@@ -13,6 +15,7 @@ def get_video_id(vid_links: pd.DataFrame, link_col="Link") -> list:
     Returns:
     list of link ids
     """
+    
     from urllib.parse import urlparse
     
     df = vid_links.copy()
@@ -21,5 +24,61 @@ def get_video_id(vid_links: pd.DataFrame, link_col="Link") -> list:
     df["query"] = df["url_dat"].swifter.apply(qurery_func)
     get_id_func = lambda x: x[2:]
     df["vid_id"] = df["query"].swifter.apply(get_id_func)
+    
     return list(df["vid_id"])
+
+
+def get_transcripts(vid_ids: list, trans_dir: str, titles: list) -> None:
+    """
+    use youtube api gather and store transcripts 
+
+    Params:
+    vid_ids: list of video ids
+    trans_dir: directory to store transcripts
+    titles: list of video titles
+
+    Returns:
+    None
+    """
+
+    if (os.path.exists(trans_dir)) != True:
+        os.mkdir(trans_dir)
+        from youtube_transcript_api import YouTubeTranscriptApi
+        index = 0
+        get_vid_funct = lambda x: YouTubeTranscriptApi.get_transcript(str(x))
+        for id in vid_ids:
+            try:
+                transcript = get_vid_funct(id)
+                title = titles[index]
+                print(id)
+                print(title)
+                with open(trans_dir + "/" + title + ".json", "w") as final:
+                    json.dump(transcript, final)
+                
+            except Exception as e:
+                print(e)
+                
+            index += 1
+
+def read_in_trans(file_list: list, trans_dir: str) -> pd.DataFrame:
+    """
+    takes in list of json files and 
+    returns a merged and tagged data
+    frame consisting of all transcripts 
+
+    Params:
+    file_list: list of json files
+
+    Returns:
+    pandas data frame of transcripts
+    """
+
+    df_list = [pd.read_json(trans_dir + "/" + filename, orient="records") \
+               for filename in file_list]
+    
+    for title, df in zip(file_list, df_list): df["vid_title"] = title[:-5]
+
+    df = pd.concat(df_list) 
+
+    return df
     
