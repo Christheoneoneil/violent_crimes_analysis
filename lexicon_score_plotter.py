74c400cfaacc7fa776a446856db12af7d7cf8cb8
kdm004 <97989597+kdm004@users.noreply.github.com>
2023-04-23 20:50:06 -0400

    Update lexicon_score_plotter.py

--- lexicon_score_plotter.py
+++ lexicon_score_plotter.py
@@ -1,242 +1,215 @@
 from bs4 import BeautifulSoup
-from collections import Counter
 import html5lib
+import requests
+
 import matplotlib.pyplot as plt
-import numpy as np
-import os
 import pandas as pd
 import re
-import requests
+import numpy as np
 import seaborn as sns
 import shifterator as sh
+from collections import Counter
+import os
+import math
 
-def Clean_Words(string):
+
+
+def Plot_Danger_Or_Power(df, plot_type):
     """
-    Description:
-        Remove formatting and punctuation from source.
+    Description: 
+    Creates a grid of plots for each unique name in the dataframe,
+    with 'danger' on the y-axis and word number on the x-axis.
+
+    params: 
+            df = pandas dataframe of name, token, danger, power
+            plot_type = 'danger' or 'power'
+
+    returns: 
+            none
+    """
+    plt.style.use('dark_background')
+
+    # define color for each plot type
+    if plot_type == 'danger':
+        color = 'crimson'
+    if plot_type == 'power':
+        color = 'darkorchid'
+
+    # plot by name
+    groups = df.groupby('name')
+
+    # create plot layout
+    n = len(groups)
+    nrows = math.ceil(math.sqrt(n))
+    ncols = math.ceil(n/nrows)
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(12,8))
+    axs = axs.ravel()
     
-    Parameters:
-        string: str, String of input text.
+    # iterate over each group and create a plot for each name
+    for i, (name, group) in enumerate(groups):
+        # get the x and y values for the plot
+        x = range(1, len(group)+1)
+        y = group[plot_type]
+        
+        # plot the data on the appropriate subplot
+        ax = axs[i]
+        ax.plot(x, y, color=color)
+        
+        # Set the title and labels for the subplot
+        ax.set_title(name)
+        ax.set_xlabel('word number')
+        ax.set_ylabel(plot_type)
+    
+    # remove unused subplots from the grid
+    for j in range(i+1, nrows*ncols):
+        fig.delaxes(axs[j])
 
-    Returns:
-        list[str], String of tokenized text.
-    """
-    # Define regular expressions to match words, punctuation, and sequences of dots
+    # modify plot layout
+    plt.tight_layout()
+    
+    # save plot to disk
+    plt.savefig('my_'+str(plot_type)+'.png')
+    
+
+
+
+def Get_Tokens_List(book_name):
+
+    '''
+    Description: 
+    Tokenizes words of a given text
+            
+    Params: 
+            Filename.txt
+            
+    Returns: 
+            List of words/tokens in the .txt file
+    '''
+    
+    file = open(book_name, 'r') 
+    text = file.read()
+
     word_regex = r'\b\w+\b'
     punct_regex = r'[^\w\s]'
     #dot_regex = r'\.+'
     dot_regex = r'\.{4,}|\.{3}|\.{2}|\.'
+    text = text.replace('\n', ' ')
+    text = text.split(' ')
+    text = [entry for entry in text if entry.strip()]
+    text = [entry.lower() for entry in text]
+    text_string = ' '.join(text)
 
-    # Define a list to hold the resulting tokens
+    # define a list to hold the resulting tokens
     tokens = []
-
-    # Iterate over the matches of the regular expressions and append them to the tokens list
-    for match in re.finditer(f'{dot_regex}|{punct_regex}|{word_regex}', string):
+    # iterate over the matches of the regular expressions and append them to the tokens list
+    for match in re.finditer(f'{dot_regex}|{punct_regex}|{word_regex}', text_string):
         token = match.group()
-
         if re.match(word_regex, token):
             tokens.append([token])
-
         elif re.match(dot_regex, token):
             tokens.append([token])
-
         else:
-            # Split the token into parts that consist of either dots or non-dots
+            # split the token into parts that consist of either dots or non-dots
             parts = re.findall(f'(?:{dot_regex})+|[^\.\s]+', token)
             for part in parts:
                 tokens.append([part])
+    tokens_list = [''.join(entry) for entry in tokens]
 
-    return [''.join(entry) for entry in tokens]
+    return tokens_list
 
 
-def Get_Tokens_List(book_name):
-    """
-    Description:
-        Initial extraction of strings from source.
-    
-    Parameters:
-        book_name: str, File name and extension for source data.
-
-    Returns:
-        Clean_Words(text_string): list[str], String of tokenized text.
-    """
-    file = open(book_name, 'r') 
-    text = file.read()
-
-    # replace all \n with ' ' 
-    text = text.replace('\n', ' ')
-
-    # split everything into entries of a list
-    text = text.split(' ')
-
-    # Remove all ' ' entries
-    text = [entry for entry in text if entry.strip()]
-
-    # make everything lowercase
-    text = [entry.lower() for entry in text]
-    
-    text_string = ' '.join(text)
-
-    # Combine new_res elements into a string with spaces for separators
-    # Get a list of all tokens by calling Clean_Words on text_string
-    return Clean_Words(text_string)
-
-def get_happiness_scores(ngrams, hedon, hap_vars): 
-    """
-    Description:
-        .
-    
-    Parameters:
-        ngrams: str, Tokenized input text.
-        hedon: dict{str : float}, LabMT sentiment scores for english words.
-        hap_vars: list[list[str], list[float]], 
-                Index 0: List of words as strings;
-                Index 1: List of scores ad floats.
-
-    Returns:
-        list[float], List of scores as floats.
-    """
-    # hap_vars are hap_vars[0] = 'words' and hap_vars[1]='scores'
-    hap_df = hedon[hap_vars]
-    hap_dict = {key:val for key, val in zip(hap_df[hap_vars[0]], hap_df[hap_vars[1]])}
-    return [hap_dict[gram] if gram in list(hap_dict.keys()) else 0 for gram in ngrams]
 
 def Get_Token_Scores(tokens, df_lexicon_scores, score_type):
-    """
-    Description:
-        Extract danger scores for tokenized text.
-    
-    Parameters:
-        tokens: str, Tokenized input text.
-        df_lexicon_scores: dict{str : float}, LabMT scores for english words.
-        score_type: str, Desired sentiment to score.
 
-    Returns:
-        list[dict{str : float}], List of dictionary with word-score pairs.
-    """
+    '''
+    Description: Gets the token scores from the lexicon 
+    
+    Params: 
+            tokens: list of tokenized text 
+            lexicon: pandas dataframe
+            score_type: string
+    
+    Returns: 
+            List of the scores that correspond to the tokens list
+    
+    '''
+
     # create a dataframe from the tokens list
     df_tokens = pd.DataFrame({'word': tokens})
     
-    # merge the tokens dataframe with df_lexicon_scores
-    # score_type can be power, danger, etc...
-    # return the danger scores as a list
-    return pd.merge(df_tokens, df_lexicon_scores[['word', score_type]], on='word', how='left')['danger'].tolist() 
-
-def calculate_averages(raw_scores,window_size):
-    """
-    Description:
-        .
+    # merge the tokens dataframe with df_lexicon_scores 
+    df_scores = pd.merge(df_tokens, df_lexicon_scores[['word', score_type]], on='word', how='left')   # score_type can be power, danger, etc...
     
-    Parameters:
-        .
+    # replace scores for words not in lexicon with np.nan
+    df_scores[score_type] = np.where(df_scores[score_type].isnull() & df_scores['word'].isin(tokens), np.nan, df_scores[score_type])
 
-    Returns:
-        .
-    """
-    raw_series = pd.Series(raw_scores)
-    windows = raw_series.rolling(window_size)
-    avgs = windows.mean()
-    return avgs
+    # return the scores as a list
+    return df_scores[score_type].tolist()
 
-def plotter(data: list, titles: str, rows: int, cols: int):   
+
+
+def Create_Score_DataFrame(lexicon, folder_path, file_names):
     """
-    Description:
-        .
+    Description: 
+            Creates a dataframe of name, token, danger, power
     
-    Parameters:
-        .
-
-    Returns:
-        .
-    """ 
-    fig, axs = plt.subplots(nrows=rows, ncols=cols, figsize=(8, 10))
-    fig.tight_layout()
-    sns.despine(fig)
-    
-    for avg, ax, title in zip(data, axs.ravel(), titles):
-        ax.plot(avg)
-        ax.set_title(title, fontsize=7)
-        ax.set_ylabel(r"$h_{avg}$", fontsize=7)
-        ax.tick_params(axis='both', which='minor', labelsize=5)
-    axs.ravel()[-1].set_xlabel("Word number i")
-
-
-def word_shifts(type2freq_1: dict, type2freq_2: dict, ref_avg: float, title: list):
+    params:
+            lexicon: pandas dataframe
+            folder_path: string
+            file_names: list of strings
+            
+    returns:
+            scores_df: pandas dataframe
     """
-    Description:
-        .
-    
-    Parameters:
-        .
 
-    Returns:
-        .
-    """
-    sentiment_shift = sh.WeightedAvgShift(type2freq_1=type2freq_1,
-                                      type2freq_2=type2freq_2,
-                                      type2score_1='labMT_English',
-                                      reference_value=ref_avg,
-                                      stop_lens=[(4,6)])
-    sentiment_shift.get_shift_graph(detailed=True,
-                                system_names=[title[0], title[1]])
-
-def Create_Score_Dictionary(score_type):
-    """
-    Description:
-        .
-    
-    Parameters:
-        .
-
-    Returns:
-        .
-    """
-    # read in lexicon
-    df_lexicon_scores = pd.read_table('ousiometry_data_augmented.tsv')
-
-    # create an empty dictionary to store the results
-    results_dict = {}
-
-    # get a list of the .txt files in the 'shooters_words_text' directory
-    folder_path = 'shooters_words_text'
-    file_names = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
-
-    print(file_names)
+    # create an empty list to store the results
+    results = []
 
     # loop over the file names and apply the function to each file
     for file_name in file_names:
+
         # apply the function to the file contents
         tokens_list = Get_Tokens_List(folder_path + '/' + file_name)
 
-        "We could probably just add the following line:"
-        # Get_Token_Scores(tokens_list, df_lexicon_scores)
+        # get the scores for each token using the lexicon
+        danger_scores_list = Get_Token_Scores(tokens_list, lexicon, 'danger')
+        power_scores_list = Get_Token_Scores(tokens_list, lexicon, 'power')
 
-        tokens_list = Get_Token_Scores(tokens_list, df_lexicon_scores, score_type)
-        # store the output in the results dictionary with the file name as the key
-        criminal_name = file_name.split('.')[0]
-        results_dict[criminal_name] = tokens_list
+        # create a DataFrame with the results for this file
+        file_df = pd.DataFrame({'name': [file_name.strip('.txt')]*len(tokens_list),
+                                'token': tokens_list,
+                                'danger': danger_scores_list,
+                                'power': power_scores_list})
 
-    # print out the results dictionary
-    print(results_dict)
-    return results_dict
+        # append the file_df to the list of results
+        results.append(file_df)
 
-if __name__ == "__main__":
-    danger_dict = Create_Score_Dictionary('danger')
-    danger_scores1 = list(danger_dict.values())[1] # this is for the first criminal in our list of criminal_names
-    danger_names1 = list(danger_dict.values())[0]
+    # concatenate all the results into a single DataFrame
+    scores_df = pd.concat(results)
 
-    "Now, let's use these lists in order to create a danger plot."
-    # for name, score_list in danger_dict.items():
-    #     "TODO: Insert function for plotting the score_list"
-    # list(results_dict.values())
+    return scores_df
 
-    # get danger scores for the first criminal
-    a = 1
-    b = 4.5
-    c = .5
 
-    window_sizes = [round(10**i) for i in np.arange(1, 4.5, .5)] 
-    rolling_averages = [calculate_averages(danger_scores1, window_size) for window_size in window_sizes] 
-    t_list = ["Insert Criminal Name Here, T = " + str(window_size) + ", z = " + str(round(np.log10(window_size)*2)/2) for window_size in window_sizes]
-    plotter(rolling_averages, t_list, 7, 1)
-    plt.savefig("unadjusted_happiness.png")
+
+
+
+    
+# Actually create the plots...
+
+df_lexicon_scores = pd.read_table('ousiometry_data_augmented.tsv')
+folder_path = 'shooters_words_text'
+file_names = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
+
+df = Create_Score_DataFrame(df_lexicon_scores, folder_path, file_names)
+Plot_Danger_Or_Power(df, 'danger')
+Plot_Danger_Or_Power(df, 'power')
+
+
+
+
+
+
+
+
+
+
