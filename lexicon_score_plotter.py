8956c8647f4f7c7b711b746dd1cfff76ed918a54
kdm004 <97989597+kdm004@users.noreply.github.com>
2023-04-28 19:09:16 -0400

    Update lexicon_score_plotter.py

--- lexicon_score_plotter.py
+++ lexicon_score_plotter.py
@@ -1,215 +1,160 @@
-from bs4 import BeautifulSoup
-import html5lib
-import requests
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
 import numpy as np
 import seaborn as sns
-import shifterator as sh
-from collections import Counter
 import os
-import math
+import c
 
-
-
-def Plot_Danger_Or_Power(df, plot_type):
+def plotter(desired_params:list, ref_col:str, df:pd.DataFrame, tickers:list, ncols:int, title:str, colors:list)->None:
     """
-    Description: 
-    Creates a grid of plots for each unique name in the dataframe,
-    with 'danger' on the y-axis and word number on the x-axis.
-
-    params: 
-            df = pandas dataframe of name, token, danger, power
-            plot_type = 'danger' or 'power'
-
-    returns: 
-            none
+    Description:
+    Creates a grid of subplots for a given dataframe
+    
+    Params:
+    desired_parms: list of params for seperate subplots
+    ref_col: groupby column 
+    df: dataframe for plotting
+    tickers: given values for each plot
+    ncols: number of desired columns
+    title: super title of plot
+    color: list of colors for plot
+    
+    Returns:
+    None
     """
     plt.style.use('dark_background')
-
-    # define color for each plot type
-    if plot_type == 'danger':
-        color = 'crimson'
-    if plot_type == 'power':
-        color = 'darkorchid'
-
-    # plot by name
-    groups = df.groupby('name')
-
-    # create plot layout
-    n = len(groups)
-    nrows = math.ceil(math.sqrt(n))
-    ncols = math.ceil(n/nrows)
-    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(12,8))
-    axs = axs.ravel()
-    
-    # iterate over each group and create a plot for each name
-    for i, (name, group) in enumerate(groups):
-        # get the x and y values for the plot
-        x = range(1, len(group)+1)
-        y = group[plot_type]
-        
-        # plot the data on the appropriate subplot
-        ax = axs[i]
-        ax.plot(x, y, color=color)
-        
-        # Set the title and labels for the subplot
-        ax.set_title(name)
-        ax.set_xlabel('word number')
-        ax.set_ylabel(plot_type)
-    
-    # remove unused subplots from the grid
-    for j in range(i+1, nrows*ncols):
-        fig.delaxes(axs[j])
-
-    # modify plot layout
-    plt.tight_layout()
-    
-    # save plot to disk
-    plt.savefig('my_'+str(plot_type)+'.png')
-    
-
-
-
-def Get_Tokens_List(book_name):
-
-    '''
-    Description: 
-    Tokenizes words of a given text
+    nrows = len(tickers) // ncols + (len(tickers) % ncols > 0)
+    for param, color in zip(desired_params, colors):
+            df = df.copy()
+            sub_df = df[[ref_col, param]]
+            plt.figure(figsize=(15,12))
+            plt.subplots_adjust(hspace=0.2)
+            plt.suptitle(param.title() + title.title())
+            for n, tick in enumerate(tickers):
+                ax = plt.subplot(nrows, ncols, n+1)
+                sub_df[sub_df[ref_col] == tick].reset_index(drop=True).plot(ax=ax, color=color, legend=False) # setting .reset_index seemed to do something...
+                ax.axhline(y=0, c="lightsteelblue", linestyle="--")
+                ax.get_xaxis().set_visible(False)
+                ax.set_title(tick)
+                sns.despine()
             
-    Params: 
-            Filename.txt
-            
-    Returns: 
-            List of words/tokens in the .txt file
-    '''
-    
-    file = open(book_name, 'r') 
-    text = file.read()
-
-    word_regex = r'\b\w+\b'
-    punct_regex = r'[^\w\s]'
-    #dot_regex = r'\.+'
-    dot_regex = r'\.{4,}|\.{3}|\.{2}|\.'
-    text = text.replace('\n', ' ')
-    text = text.split(' ')
-    text = [entry for entry in text if entry.strip()]
-    text = [entry.lower() for entry in text]
-    text_string = ' '.join(text)
-
-    # define a list to hold the resulting tokens
-    tokens = []
-    # iterate over the matches of the regular expressions and append them to the tokens list
-    for match in re.finditer(f'{dot_regex}|{punct_regex}|{word_regex}', text_string):
-        token = match.group()
-        if re.match(word_regex, token):
-            tokens.append([token])
-        elif re.match(dot_regex, token):
-            tokens.append([token])
-        else:
-            # split the token into parts that consist of either dots or non-dots
-            parts = re.findall(f'(?:{dot_regex})+|[^\.\s]+', token)
-            for part in parts:
-                tokens.append([part])
-    tokens_list = [''.join(entry) for entry in tokens]
-
-    return tokens_list
+            plt.savefig(param)
 
 
 
-def Get_Token_Scores(tokens, df_lexicon_scores, score_type):
-
-    '''
-    Description: Gets the token scores from the lexicon 
-    
-    Params: 
-            tokens: list of tokenized text 
-            lexicon: pandas dataframe
-            score_type: string
-    
-    Returns: 
-            List of the scores that correspond to the tokens list
-    
-    '''
-
-    # create a dataframe from the tokens list
-    df_tokens = pd.DataFrame({'word': tokens})
-    
-    # merge the tokens dataframe with df_lexicon_scores 
-    df_scores = pd.merge(df_tokens, df_lexicon_scores[['word', score_type]], on='word', how='left')   # score_type can be power, danger, etc...
-    
-    # replace scores for words not in lexicon with np.nan
-    df_scores[score_type] = np.where(df_scores[score_type].isnull() & df_scores['word'].isin(tokens), np.nan, df_scores[score_type])
-
-    # return the scores as a list
-    return df_scores[score_type].tolist()
 
 
-
-def Create_Score_DataFrame(lexicon, folder_path, file_names):
+def graph_power_danger(criminal_df:pd.DataFrame) -> None:
     """
-    Description: 
-            Creates a dataframe of name, token, danger, power
+    Description:
+    Uses ousiometrics to graph power and danger curves as functions of word index
+
+    Params:
+    criminal_df: data frame of all criminal transcripts
     
-    params:
-            lexicon: pandas dataframe
-            folder_path: string
-            file_names: list of strings
-            
-    returns:
-            scores_df: pandas dataframe
+    Returns:
+    None
     """
 
+    criminal_df[c.words_col] = criminal_df[c.text_col].str.split()
+    words_expanded = criminal_df.explode(c.words_col) 
+    words_expanded[c.words_col] = words_expanded[c.words_col].map(str)
+    words_expanded[c.words_col] = words_expanded[c.words_col].str.lower()
+    
+    words_expanded[c.words_col] = [re.sub(r'[^\w\s]', '', word) for word in list(words_expanded[c.words_col])]
+    
+    power_danger = pd.read_table(c.ousio_dat, usecols=[c.ousiowords, c.ousiopower, c.ousiodanger])
+    word_in_lex = lambda x: True if x in list(power_danger[c.ousiowords]) else False
+    power_func = lambda x, y: list(power_danger[(power_danger == x).any(axis=1)].to_dict()[y].values())[0] if word_in_lex(x) else np.nan
+    
+    
+    words_expanded[c.ousiopower] = [power_func(word, c.ousiopower) for word in list(words_expanded[c.words_col])]
+    words_expanded[c.ousiodanger] = [power_func(word, c.ousiodanger) for word in list(words_expanded[c.words_col])]
+    words_expanded.dropna(inplace=True)
+
+    crim_list = list(words_expanded[c.crim_col].unique())
+    ncols = c.ncols
+    params = [c.ousiopower, c.ousiodanger]
+    
+    plotter(desired_params=params, ref_col=c.crim_col, df=words_expanded,     # words_expanded is being fed in as a dataframe... what's going on here? We should be plotting word score as a function of the word number
+            tickers=crim_list, ncols=ncols, title=" curves per transcript",
+            colors=c.colors)
+    
+    words_expanded.drop(columns=[c.text_col], inplace=True)
+    non_sparse = words_expanded.set_index(c.crim_col).drop(labels=['romano', 'bartley', 'castillo']) # holmes is automatically not plotted because it doesn't have tokens to put in the dataframe.
+    non_sparse.reset_index(inplace=True)
+    for param in params: non_sparse[param + " rolling avg"] = non_sparse.groupby(c.crim_col)[param].transform(lambda x: x.rolling(c.window_size).mean())
+    rolling_params = [param + " rolling avg" for param in params]
+
+    plotter(desired_params=rolling_params, ref_col=c.crim_col, df=non_sparse, 
+            tickers=list(non_sparse[c.crim_col].unique()), ncols=c.ncols, title=" per transcript",
+            colors=c.colors)
+    
+
+    
+    words_expanded.to_csv(c.final_df)
+
+
+
+def create_criminal_dataFrame(folder_path: str, file_names: list):
+    """
+    Returns a pandas DataFrame containing file names and their contents.
+    The DataFrame has columns: 'name', 'Text'
+
+    Description:
+    Creates a dataframe of criminals and the words of their text 
+
+    Params:
+    folder: folder name as a string
+    file_names: list of file names without any extensions
+
+    Returns:
+    text_df: dataframe containing two columns -- 'name' and 'Text' 
+
+
+
+    """
     # create an empty list to store the results
     results = []
 
     # loop over the file names and apply the function to each file
     for file_name in file_names:
 
-        # apply the function to the file contents
-        tokens_list = Get_Tokens_List(folder_path + '/' + file_name)
-
-        # get the scores for each token using the lexicon
-        danger_scores_list = Get_Token_Scores(tokens_list, lexicon, 'danger')
-        power_scores_list = Get_Token_Scores(tokens_list, lexicon, 'power')
+        # read the file contents
+        with open(folder_path + '/' + file_name, 'r') as f:
+            text = f.read()
 
         # create a DataFrame with the results for this file
-        file_df = pd.DataFrame({'name': [file_name.strip('.txt')]*len(tokens_list),
-                                'token': tokens_list,
-                                'danger': danger_scores_list,
-                                'power': power_scores_list})
+        file_df = pd.DataFrame({'name': [file_name.strip('.txt')],
+                                'Text': [text]})
 
         # append the file_df to the list of results
         results.append(file_df)
 
-    # concatenate all the results into a single DataFrame
-    scores_df = pd.concat(results)
+    # concatenate all the results into a single dataframe
+    text_df = pd.concat(results)
 
-    return scores_df
-
-
+    return text_df
 
 
 
-    
-# Actually create the plots...
 
+
+
+
+#--------------defining variables--------------
 df_lexicon_scores = pd.read_table('ousiometry_data_augmented.tsv')
 folder_path = 'shooters_words_text'
 file_names = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
+criminal_names = [f.strip('.txt') for f in os.listdir(folder_path) if f.endswith('.txt')]
+df = create_criminal_dataFrame(folder_path, file_names)
+# print(file_names)
+# print(criminal_names)
+# print(df)
 
-df = Create_Score_DataFrame(df_lexicon_scores, folder_path, file_names)
-Plot_Danger_Or_Power(df, 'danger')
-Plot_Danger_Or_Power(df, 'power')
-
-
-
-
-
-
-
-
+#--------------Generating Plots--------------
+graph_power_danger(df)
 
 
