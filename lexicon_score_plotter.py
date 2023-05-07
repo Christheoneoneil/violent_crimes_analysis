da9e7c15dd9596d3a534a64dd09852b19af8aad2
kdm004 <97989597+kdm004@users.noreply.github.com>
2023-05-07 13:00:53 -0400

    Update lexicon_score_plotter.py

--- lexicon_score_plotter.py
+++ lexicon_score_plotter.py
@@ -26,6 +26,7 @@ def plotter(desired_params:list, ref_col:str, df:pd.DataFrame, tickers:list, nco
     """
     plt.style.use('dark_background')
     nrows = len(tickers) // ncols + (len(tickers) % ncols > 0)
+    plt.rc('font', size=c.font_size) 
     for param, color in zip(desired_params, colors):
             df = df.copy()
             sub_df = df[[ref_col, param]]
@@ -34,10 +35,13 @@ def plotter(desired_params:list, ref_col:str, df:pd.DataFrame, tickers:list, nco
             plt.suptitle(param.title() + title.title())
             for n, tick in enumerate(tickers):
                 ax = plt.subplot(nrows, ncols, n+1)
-                sub_df[sub_df[ref_col] == tick].reset_index(drop=True).plot(ax=ax, color=color, legend=False) # setting .reset_index seemed to do something...
-                ax.axhline(y=0, c="lightsteelblue", linestyle="--")
-                ax.get_xaxis().set_visible(False)
+                ax.plot(sub_df[sub_df[ref_col] == tick][param].to_list(), c=color)
+                score_mean = np.nanmean(sub_df[sub_df[ref_col] == tick][param].to_list())
+                yvals = [0, score_mean]
+                for axline, d_color, l_s in zip(yvals, c.dash_colrs, c.lin_styles):
+                     ax.axhline(y=axline, c=d_color, linestyle=l_s, label=round(axline,2))
                 ax.set_title(tick)
+                ax.legend()
                 sns.despine()
             
             plt.savefig(param)
@@ -69,7 +73,6 @@ def graph_power_danger(criminal_df:pd.DataFrame) -> None:
     word_in_lex = lambda x: True if x in list(power_danger[c.ousiowords]) else False
     power_func = lambda x, y: list(power_danger[(power_danger == x).any(axis=1)].to_dict()[y].values())[0] if word_in_lex(x) else np.nan
     
-    
     words_expanded[c.ousiopower] = [power_func(word, c.ousiopower) for word in list(words_expanded[c.words_col])]
     words_expanded[c.ousiodanger] = [power_func(word, c.ousiodanger) for word in list(words_expanded[c.words_col])]
     words_expanded.dropna(inplace=True)
@@ -78,18 +81,18 @@ def graph_power_danger(criminal_df:pd.DataFrame) -> None:
     ncols = c.ncols
     params = [c.ousiopower, c.ousiodanger]
     
-    plotter(desired_params=params, ref_col=c.crim_col, df=words_expanded,     # words_expanded is being fed in as a dataframe... what's going on here? We should be plotting word score as a function of the word number
-            tickers=crim_list, ncols=ncols, title=" curves per transcript",
+    plotter(desired_params=params, ref_col=c.crim_col, df=words_expanded, 
+            tickers=crim_list, ncols=ncols, title=" curves per manuscript",
             colors=c.colors)
     
     words_expanded.drop(columns=[c.text_col], inplace=True)
-    non_sparse = words_expanded.set_index(c.crim_col).drop(labels=['romano', 'bartley', 'castillo']) # holmes is automatically not plotted because it doesn't have tokens to put in the dataframe.
+    non_sparse = words_expanded.set_index(c.crim_col).drop(labels=c.sparse_rows)
     non_sparse.reset_index(inplace=True)
     for param in params: non_sparse[param + " rolling avg"] = non_sparse.groupby(c.crim_col)[param].transform(lambda x: x.rolling(c.window_size).mean())
     rolling_params = [param + " rolling avg" for param in params]
 
     plotter(desired_params=rolling_params, ref_col=c.crim_col, df=non_sparse, 
-            tickers=list(non_sparse[c.crim_col].unique()), ncols=c.ncols, title=" per transcript",
+            tickers=list(non_sparse[c.crim_col].unique()), ncols=c.ncols, title=" per manuscript",
             colors=c.colors)
     
 
@@ -158,3 +161,4 @@ df = create_criminal_dataFrame(folder_path, file_names)
 graph_power_danger(df)
 
 
+    
